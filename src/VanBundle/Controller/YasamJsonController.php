f91d94aa9095d1611dcb80fd0c1ae5b304758f6b
grandmasteraziz@hotmail.com <q1w2e3@>
2017-01-27 13:47:34 +0300

    emlak bitti

---
+++ src/VanBundle/Controller/YasamJsonController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace VanBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class YasamJsonController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('', array('name' => $name));
+    }
+}
